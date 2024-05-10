b0ba0ce065dcdd5af38d3988ae1981fcfcc59af1
Thirumalesh <thirumaleswara.malapati@global-csg.com>
2024-05-10 13:34:56 +0530

    day1 code

---
+++ day1/Oparators.py
@@ -0,0 +1,32 @@
+# Types of Oparetors
+''' A oparetor is a symbol that perform certain oparation between operands
+* Arthemetic Oparetors (+, - , * , % , / , **)
+* Relational/ Comparisan Oparator (==, !=, <, >, <=, >=)
+* Assainment Oparetors (=, +=, -=, *=, /=, %=, **=)
+* Logical Oparetors (or, not, and)'''
+# a = 2
+# b = 3
+# sum = a+b
+# print(sum)
+
+# a = 10
+# b = 20
+
+# print((a==b) and (a<b))
+# print((a==b) or (a<b))
+# print((a!=b) or (a<b))
+# print((a!=b) and (a<b))
+
+"""Type casting and type conversion
+Type conversion is defult in python and type casting is a manual changes"""
+
+# example 
+a = 4 
+b = 6.34
+print(a+b)  # In this senario python will convert int to float the output is 10.34
+ 
+ # example 2
+ 
+c = "4"
+d = 6 
+print(int(c)+d) # hear we need to convert str in to int manually other wise we will get the error this is called type casting 
