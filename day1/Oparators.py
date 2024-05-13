301518d19cd7e9ae63a2652b4f6cf56d5dc984b1
Thirumalesh <thirumaleswara.malapati@global-csg.com>
2024-05-13 11:53:49 +0530

    added the new code

--- day1/Oparators.py
+++ day1/Oparators.py
@@ -21,12 +21,24 @@
 Type conversion is defult in python and type casting is a manual changes"""
 
 # example 
-a = 4 
-b = 6.34
-print(a+b)  # In this senario python will convert int to float the output is 10.34
+# a = 4 
+# b = 6.34
+# print(a+b)  # In this senario python will convert int to float the output is 10.34
  
- # example 2
+#  # example 2
  
-c = "4"
-d = 6 
-print(int(c)+d) # hear we need to convert str in to int manually other wise we will get the error this is called type casting 
+# c = "4"
+# d = 6 
+# print(int(c)+d) # hear we need to convert str in to int manually other wise we will get the error this is called type casting
+
+# name = str(input("Name is:"))
+# age = int(input("Age:"))
+# marks = int(input("marks:"))
+
+# print("name is:", name)
+# print("age", age)
+# print("marks:", marks)
+
+a = int(input("enter the value a:"))
+b = int(input("enter the value B: "))
+print(a>=b)
