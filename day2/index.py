4fe7153e8aad2faa7a3e5464520773e354021235
Thirumalesh <thirumaleswara.malapati@global-csg.com>
2024-05-23 11:31:06 +0530

    added

---
+++ day2/index.py
@@ -0,0 +1,5 @@
+a = input("enter the value:")
+print(len(a))
+
+b = "total bring the ball"
+print(b.count("o")) 
---
+++ day2/string.py
@@ -0,0 +1,9 @@
+# escap secvence charactor 
+#  "\n" New line 
+#  "\t" tab space 
+
+a = "thiru name"
+b = "venky name"
+b = b
+print(a+b)
+print(len(a))
